0000ac33460166cf144aeda8757b95dc2c8ad89f
Andrew Jeffree <andrew.jeffree@safetyculture.io>
2025-03-01 10:28:01 +1100

    chore: add DelegationSet logic to get hooks and add tests

--- pkg/resource/hosted_zone/sdk.go
+++ pkg/resource/hosted_zone/sdk.go
@@ -137,6 +137,22 @@ func (rm *resourceManager) sdkFind(
 	if err := rm.setResourceAdditionalFields(ctx, ko); err != nil {
 		return nil, err
 	}
+
+	if resp.DelegationSet != nil {
+		f := &svcapitypes.DelegationSet{}
+		if resp.DelegationSet.CallerReference != nil {
+			f.CallerReference = resp.DelegationSet.CallerReference
+		}
+		if resp.DelegationSet.Id != nil {
+			f.ID = resp.DelegationSet.Id
+		}
+		if resp.DelegationSet.NameServers != nil {
+			f.NameServers = aws.StringSlice(resp.DelegationSet.NameServers)
+		}
+		ko.Status.DelegationSet = f
+	} else {
+		ko.Status.DelegationSet = nil
+	}
 	return &resource{ko}, nil
 }
 
--- test/e2e/tests/test_hosted_zone.py
+++ test/e2e/tests/test_hosted_zone.py
@@ -109,6 +109,23 @@ class TestHostedZone:
         route53_validator = Route53Validator(route53_client)
         route53_validator.assert_hosted_zone(zone_id)
 
+    def test_delegation_set(self, route53_client, public_hosted_zone):
+        ref, cr = public_hosted_zone
+
+        resource = k8s.get_resource(ref)
+        resource_id = cr["status"]["id"]
+
+        time.sleep(CREATE_WAIT_AFTER_SECONDS)
+
+        # Check hosted_zone exists in AWS
+        route53_validator = Route53Validator(route53_client)
+        route53_validator.assert_hosted_zone(resource_id)
+
+        assert resource["status"]["delegationSet"] is not None
+        assert resource["status"]["delegationSet"]["callerReference"]
+        assert resource["status"]["delegationSet"]["id"]
+        assert len(resource["status"]["delegationSet"]["nameServers"]) > 0
+
     @pytest.mark.resource_data({'tag_key': 'initialtagkey', 'tag_value': 'initialtagvalue'})
     def test_crud_tags(self, route53_client, public_hosted_zone):
         ref, cr = public_hosted_zone
