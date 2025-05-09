9b1350e4ae04ad7154d333f8050bd3022a8c8d52
Michael Tewoldemedhin <98621731+michaelhtm@users.noreply.github.com>
2025-05-09 14:23:36 -0700

    fix: populate recordSetName during adoption (#74)
    Issue [#2187](https://github.com/aws-controllers-k8s/community/issues/2187)
    
    Description of changes:
    Currently the recordSetName does not get populated to the 
    resource manifest, which makes it impossible for user to be 
    able to adopt recordSets with certain types.
    
    Tested these changes locally when adopting CNAME recordType
    
    Here's how you would adopt a resource now:
    ```yaml
    apiVersion: route53.services.k8s.aws/v1alpha1
    kind: RecordSet
    metadata:
      name: myrecordSet
      annotations:
        services.k8s.aws/deletion-policy: retain
        services.k8s.aws/adoption-policy: adopt
        services.k8s.aws/adoption-fields: |
          {
            "id": "<RecordSet ID>",
            "hostedZoneID": "<hostedZoneID>",
            "recordType": "CNAME",
            "name": "<RecordSetName>"
          }
    ```
    By submitting this pull request, I confirm that my contribution is made under the terms of the Apache 2.0 license.

--- pkg/resource/record_set/resource.go
+++ pkg/resource/record_set/resource.go
@@ -102,6 +102,11 @@ func (r *resource) SetIdentifiers(identifier *ackv1alpha1.AWSIdentifiers) error 
 	if f1ok {
 		r.ko.Spec.RecordType = &f1
 	}
+
+	if f2, f2ok := identifier.AdditionalKeys["name"]; f2ok {
+		r.ko.Spec.Name = &f2
+	}
+
 	return nil
 }
 
@@ -120,6 +125,10 @@ func (r *resource) PopulateResourceFromAnnotation(fields map[string]string) erro
 
 	if f1, f1ok := fields["recordType"]; f1ok {
 		r.ko.Spec.RecordType = &f1
+	}
+
+	if f2, f2ok := fields["name"]; f2ok {
+		r.ko.Spec.Name = &f2
 	}
 
 	return nil
--- templates/hooks/record_set/post_set_resource_identifiers.go.tpl
+++ templates/hooks/record_set/post_set_resource_identifiers.go.tpl
@@ -2,3 +2,7 @@
 	if f1ok {
 		r.ko.Spec.RecordType = &f1
 	}
+
+	if f2, f2ok := identifier.AdditionalKeys["name"]; f2ok {
+		r.ko.Spec.Name = &f2
+	}
