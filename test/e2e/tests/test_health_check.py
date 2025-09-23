393e5ef9f7d22fd6fc872a2c1fdd538d79359e38
Gustavo Diaz <gustidia@amazon.com>
2025-09-23 23:56:55 +0000

    tests: Ready condition rename (ACK.ResourceSynced→Ready, assert_*_synced→assert_*_ready)

--- test/e2e/tests/test_health_check.py
+++ test/e2e/tests/test_health_check.py
@@ -151,7 +151,7 @@ class TestHealthCheck:
         time.sleep(MODIFY_WAIT_AFTER_SECONDS)
 
         # Check resource synced successfully
-        assert k8s.wait_on_condition(ref, "ACK.ResourceSynced", "True", wait_periods=5)
+        assert k8s.wait_on_condition(ref, "Ready", "True", wait_periods=5)
 
         # Check for updated user tags; system tags should persist
         health_check = route53_validator.list_tags_for_resources(resource_id, "healthcheck")
@@ -182,7 +182,7 @@ class TestHealthCheck:
         time.sleep(MODIFY_WAIT_AFTER_SECONDS)
 
         # Check resource synced successfully
-        assert k8s.wait_on_condition(ref, "ACK.ResourceSynced", "True", wait_periods=5)
+        assert k8s.wait_on_condition(ref, "Ready", "True", wait_periods=5)
 
         # Check for removed user tags; system tags should persist
         health_check = route53_validator.list_tags_for_resources(resource_id, "healthcheck")
