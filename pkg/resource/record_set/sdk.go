c8f8d7f5fc9651c3a7f0e0b18cb585e882618e75
Trevor Knottnerus <tknott@amazon.com>
2026-05-06 15:48:44 -0700

    Update sdkFind to no longer for Synced to False for nil Status.ID

--- pkg/resource/record_set/sdk.go
+++ pkg/resource/record_set/sdk.go
@@ -283,12 +283,19 @@ func (rm *resourceManager) sdkFind(
 	// Status represents whether record changes have been fully propagated to all
 	// Route 53 authoritative DNS servers. The current status for the propagation
 	// should be updated if it's not already INSYNC.
-	err = rm.syncStatus(ctx, ko)
-	if err != nil {
-		return nil, err
-	}
-	if ko.Status.Status == nil || svcsdktypes.ChangeStatus(*ko.Status.Status) != svcsdktypes.ChangeStatusInsync {
-		ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, nil, nil)
+	//
+	// If there is no change ID (e.g. the resource was adopted rather than
+	// created/updated by this controller), we skip the propagation check.
+	// The record was confirmed to exist via ListResourceRecordSets, so it is
+	// already present on the authoritative servers.
+	if ko.Status.ID != nil {
+		err = rm.syncStatus(ctx, ko)
+		if err != nil {
+			return nil, err
+		}
+		if ko.Status.Status == nil || svcsdktypes.ChangeStatus(*ko.Status.Status) != svcsdktypes.ChangeStatusInsync {
+			ackcondition.SetSynced(&resource{ko}, corev1.ConditionFalse, nil, nil)
+		}
 	}
 
 	return &resource{ko}, nil
