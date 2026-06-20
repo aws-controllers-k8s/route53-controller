3671907e6c24052fe17012f465ca6b5222fcd295
ci-robot <ci-robot@k8s.io>
2026-06-20 18:49:07 +0000

    Update to ACK runtime `v0.59.2-0.20260617181621-38bd4c967a01`, code-generator `v0.60.0`

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/aws-controllers-k8s/route53-controller
 go 1.25.0
 
 require (
-	github.com/aws-controllers-k8s/runtime v0.59.1
+	github.com/aws-controllers-k8s/runtime v0.59.2-0.20260617181621-38bd4c967a01
 	github.com/aws/aws-sdk-go v1.49.0
 	github.com/aws/aws-sdk-go-v2 v1.34.0
 	github.com/aws/aws-sdk-go-v2/service/route53 v1.48.3
--- pkg/resource/record_set/references.go
+++ pkg/resource/record_set/references.go
@@ -25,6 +25,7 @@ import (
 
 	ackv1alpha1 "github.com/aws-controllers-k8s/runtime/apis/core/v1alpha1"
 	ackerr "github.com/aws-controllers-k8s/runtime/pkg/errors"
+	ackrt "github.com/aws-controllers-k8s/runtime/pkg/runtime"
 	acktypes "github.com/aws-controllers-k8s/runtime/pkg/types"
 
 	svcapitypes "github.com/aws-controllers-k8s/route53-controller/apis/v1alpha1"
@@ -111,9 +112,17 @@ func (rm *resourceManager) resolveReferenceForHealthCheckID(
 		if arr.Name == nil || *arr.Name == "" {
 			return hasReferences, fmt.Errorf("provided resource reference is nil or empty: HealthCheckRef")
 		}
-		namespace := ko.ObjectMeta.GetNamespace()
-		if arr.Namespace != nil && *arr.Namespace != "" {
-			namespace = *arr.Namespace
+		namespace, err := ackrt.ResolveCrossNamespaceReference(
+			ctx,
+			rm.cfg.EnableCrossNamespace,
+			&ko.Status.Conditions,
+			ackrt.CrossNamespaceRefKindResource,
+			ko.ObjectMeta.GetNamespace(),
+			arr.Namespace,
+			*arr.Name,
+		)
+		if err != nil {
+			return hasReferences, err
 		}
 		obj := &svcapitypes.HealthCheck{}
 		if err := getReferencedResourceState_HealthCheck(ctx, apiReader, obj, *arr.Name, namespace); err != nil {
@@ -194,9 +203,17 @@ func (rm *resourceManager) resolveReferenceForHostedZoneID(
 		if arr.Name == nil || *arr.Name == "" {
 			return hasReferences, fmt.Errorf("provided resource reference is nil or empty: HostedZoneRef")
 		}
-		namespace := ko.ObjectMeta.GetNamespace()
-		if arr.Namespace != nil && *arr.Namespace != "" {
-			namespace = *arr.Namespace
+		namespace, err := ackrt.ResolveCrossNamespaceReference(
+			ctx,
+			rm.cfg.EnableCrossNamespace,
+			&ko.Status.Conditions,
+			ackrt.CrossNamespaceRefKindResource,
+			ko.ObjectMeta.GetNamespace(),
+			arr.Namespace,
+			*arr.Name,
+		)
+		if err != nil {
+			return hasReferences, err
 		}
 		obj := &svcapitypes.HostedZone{}
 		if err := getReferencedResourceState_HostedZone(ctx, apiReader, obj, *arr.Name, namespace); err != nil {
